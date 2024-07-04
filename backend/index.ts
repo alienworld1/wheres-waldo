d1066adb401f9dd9210efeb17b9abc9a7b1bb295
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-07-04 18:13:35 +0530

    Set up backend server

---
+++ backend/index.ts
@@ -0,0 +1,46 @@
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
+import dotenv from "dotenv";
+import createHttpError from "http-errors";
+import * as mongoose from "mongoose";
+import indexRouter from "./routes/index";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.port || 3000;
+
+const mongoDB = process.env.MONGODB_URL || "";
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoDB).catch(err => console.error(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", indexRouter);
+app.use((req, res, next) => next(createHttpError(404)));
+
+const errorHandler: ErrorRequestHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  const error = req.app.get("env") === "development" ? err : {};
+  const status = (err as any).status || 500;
+
+  res.status(status).json({
+    status,
+    message: err.message,
+    error,
+  });
+};
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
